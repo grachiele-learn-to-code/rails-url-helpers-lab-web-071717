860c1adf792de8d1c2b32c0b690d726de8303e48
Gianpaul Rachiele <gianpaul817@gmail.com>
2017-08-14 16:17:44 -0400

    Done

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,17 @@
 class Student < ActiveRecord::Base
+  before_save :default_value
+
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  private
+
+  def default_value
+    if self.active.nil?
+      self.active = false
+      self.save
+    end
+  end
+
 end
